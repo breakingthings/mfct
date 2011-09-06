824fd76863011f2733fca9deb3f497070fed1e7a
breakingthings <andri.piik@gmail.com>
2011-09-06 15:52:32 +0300

    last

--- mfct/Param.cpp
+++ mfct/Param.cpp
@@ -1,13 +1,13 @@
-#include "Param.h"
-#include <memory>
 
-Param::Param(const Param &param)
-{
+#include "Param.h"
+
+Param::Param(const Param & param){
+
 	*this = param;
 }
 
-Param::Param(int value)
-{
+Param::Param(int value){
+
 	this->m_value = new int(1);
 	this->m_instance_count = new int(1);
 	*((int*)m_value) = value;
@@ -15,16 +15,8 @@ Param::Param(int value)
 	
 }
 
-Param::Param(double value)
-{
-	this->m_value = new double(1);
-	this->m_instance_count = new int(1);
-	*((double*)m_value) = value;
-	this->m_type = TYPE_DOUBLE;
-}
+Param::Param(const TCHAR * value){
 
-Param::Param(const TCHAR *value)
-{
 	size_t size = _tcslen(value) * sizeof(TCHAR) + sizeof(TCHAR);
 	this->m_value = new TCHAR[size];
 	memset(this->m_value, _T('\0'), size);
@@ -34,37 +26,31 @@ Param::Param(const TCHAR *value)
 	this->m_type = TYPE_STRING;
 }
 
-Param::~Param()
-{
-	--*m_instance_count;
-	if(*m_instance_count == 0)
-	{
-		if(m_type == TYPE_STRING)
-		{
-			delete[] this->m_value;
-		} else {
-			delete this->m_value;
-		}
-		delete this->m_instance_count;
-	} else {
-		this->m_value = NULL;
-		this->m_instance_count = NULL;
-	}
-	
+Param::Param(double value){
+
+	this->m_value = new double(1);
+	this->m_instance_count = new int(1);
+	*((double*)m_value) = value;
+	this->m_type = TYPE_DOUBLE;
 }
 
-Param::ParamType Param::GetType() const
-{
+//*
+//	* Get type of parameter
+//	* @return type of parameter
+Param::ParamType Param::GetType() const {
+
 	return this->m_type;
 }
 
-const void *Param::GetValue() const
-{
+//
+//	* @return pointer to value of parameter
+const void * Param::GetValue() const {
+
 	return this->m_value;
 }
 
-Param &Param::operator=(const Param &T)
-{
+Param & Param::operator =(const Param & T) {
+
 	 if (this != &T) {
 		 this->m_value = T.m_value;
 		 this->m_type = T.m_type;
@@ -73,3 +59,7 @@ Param &Param::operator=(const Param &T)
     }
     return *this;
 }
+
+Param::~Param(void ){
+}
+
