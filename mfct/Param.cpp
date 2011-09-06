9a2ad9cbdfc13319a8ba1ab0ebe8011daf8e6bf5
breakingthings <andri.piik@gmail.com>
2011-09-06 18:01:00 +0300

    fix

--- mfct/Param.cpp
+++ mfct/Param.cpp
@@ -1,13 +1,12 @@
-
 #include "Param.h"
 
-Param::Param(const Param & param){
-
+Param::Param(const Param & param)
+{
 	*this = param;
 }
 
-Param::Param(int value){
-
+Param::Param(int value)
+{
 	this->m_value = new int(1);
 	this->m_instance_count = new int(1);
 	*((int*)m_value) = value;
@@ -15,8 +14,8 @@ Param::Param(int value){
 	
 }
 
-Param::Param(const TCHAR * value){
-
+Param::Param(const TCHAR *value)
+{
 	size_t size = _tcslen(value) * sizeof(TCHAR) + sizeof(TCHAR);
 	this->m_value = new TCHAR[size];
 	memset(this->m_value, _T('\0'), size);
@@ -34,22 +33,21 @@ Param::Param(double value){
 	this->m_type = TYPE_DOUBLE;
 }
 
-//*
-//	* Get type of parameter
-//	* @return type of parameter
-Param::ParamType Param::GetType() const {
+
+Param::ParamType Param::GetType() const
+{
 
 	return this->m_type;
 }
 
-//
-//	* @return pointer to value of parameter
-const void * Param::GetValue() const {
+const void *Param::GetValue() const
+{
 
 	return this->m_value;
 }
 
-Param & Param::operator =(const Param & T) {
+Param &Param::operator =(const Param & T) 
+{
 
 	 if (this != &T) {
 		 this->m_value = T.m_value;
